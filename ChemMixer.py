a5264827872ff5155633a3df012e103461b69d4b
Seggan <49963543+Seggan@users.noreply.github.com>
2019-06-05 15:54:32 -0400

    Final release

--- ChemMixer.py
+++ ChemMixer.py
@@ -1,162 +1,174 @@
 from game_funcs import *
-from time import sleep
-from json import load
-from sys import exit
-add_dict=dict()
-remove_dict=dict()
-creative_beaker=CreativeBeaker()
-discovery_beaker=DiscoveryBeaker()
-with open("chemicals.json",'r') as c:
-    chemdict=load(c)
-temperature=19
-print('''ChemMixer 3.3.0
+from tkinter import *
+from json import load, dump
+import os
+import sys
 
-What's new:
-fixed some bugs,
-added discovery mode,
-added aluminum derivatives,
-added more reactions,
-added credits to Drew Drew us
-That's all.\n''')
-print('''Welcome to ChemMixer, a chemistry lab simulator.
-Let's get started.''')
-mode=codify(input('''\nWhich mode?
-1)Creative Mode
-2)Discovery Mode
-'''))
-if mode=="1":
-    x=1
-    for n in chemdict:
-        add_dict[x]=n
-        x+=1
-    while True:
-        creative_beaker.cont=False
-        cmd=codify(input('''\n1)Add a chemical
-2)Remove a chemical
-3)Change the temperature
-4)Exit
-5)Empty the beaker
-\nType in the number of the command you want to execute: '''))
-        if cmd=="1":
-            msg="\nWhat chemical should I add?"
-            for n in add_dict:
-                msg+="\n"+str(n)+")"+add_dict[n]
-            cmd2=codify(input(msg+"\n"))
-            chem=add_dict[int(cmd2)]
-            creative_beaker.add(chem,chemdict)
-        elif cmd=="2":
-            msg="\nWhat chemical should I remove?"
-            x=1
-            remove_dict=dict()
-            for n in creative_beaker.chemicals:
-                remove_dict[x]=n
-                x+=1
-            for n in remove_dict:
-                msg+="\n"+str(n)+")"+remove_dict[n]
-            cmd2=codify(input(msg+"\n"))
-            chem=remove_dict[int(cmd2)]
-            creative_beaker.extract(chem)
-        elif cmd=="3":
-            cmd2=codify(input("\nWhat temperature should I set the beaker to? "))
-            try:
-                if int(cmd2)<-273:
-                    print("\nPlease enter a number larger than -274.")
-                    continue
-                if int(cmd2)>3500:
-                    print("\nThe beaker melted! Starting with a new one.")
-                    creative_beaker.reset()
-                    continue
-                temperature=int(cmd2.rstrip())
-                change_states(temperature,chemdict)
-            except TypeError:
-                print("\nPlease enter a number.")
-        elif cmd=="4":
-            print("\nCredits:")
-            sleep(1)
-            print("Idea based on: BEAKER, by THIX; CHEMIST, by THIX")
-            sleep(1)
-            print("Programmer: Daniel K.")
-            sleep(1)
-            print("Testers: Lianna K., Irina K., Daniel K., Drew Drew us")
-            sleep(1)
-            exit()
-        elif cmd=="5":
-            creative_beaker.reset()
-            print("The beaker is now empty.")
-        else:
-            print("Sorry, but",cmd,"is not a valid command. Please try again.")
-        if not creative_beaker.cont:
-            for n in range(0,100):
-                creative_beaker.react(temperature,chemdict)
-            creative_beaker.contents(temperature,chemdict)
-else:
-    while True:
-        x=1
-        for n in discovery_beaker.discovered_chemicals:
-            add_dict[x]=n
-            x+=1
-        discovery_beaker.cont=False
-        cmd=codify(input('''\n1)Add a chemical
-2)Remove a chemical
-3)Change the temperature
-4)Exit
-5)Empty the beaker
-\nType in the number of the command you want to execute: '''))
-        if cmd=="1":
-            msg="\nWhat chemical should I add?"
-            for n in add_dict:
-                msg+="\n"+str(n)+")"+add_dict[n]
-            cmd2=codify(input(msg+"\n"))
-            chem=add_dict[int(cmd2)]
-            discovery_beaker.add(chem,chemdict)
-        elif cmd=="2":
-            msg="\nWhat chemical should I remove?"
-            x=1
-            remove_dict=dict()
-            for n in discovery_beaker.chemicals:
-                remove_dict[x]=n
-                x+=1
-            for n in remove_dict:
-                msg+="\n"+str(n)+")"+remove_dict[n]
-            cmd2=codify(input(msg+"\n"))
-            chem=remove_dict[int(cmd2)]
-            discovery_beaker.extract(chem)
-        elif cmd=="3":
-            cmd2=codify(input("\nWhat temperature should I set the beaker to? "))
-            try:
-                if int(cmd2)<-273:
-                    print("\nPlease enter a number larger than -274.")
-                    continue
-                if int(cmd2)>3500:
-                    print("\nThe beaker melted! Starting with a new one.")
-                    discovery_beaker.reset()
-                    continue
-                temperature=int(cmd2.rstrip())
-                change_states(temperature,chemdict)
-            except TypeError:
-                print("\nPlease enter a number.")
-        elif cmd=="4":
-            print("\nCredits:")
-            sleep(1)
-            print("Idea based on: BEAKER, by THIX; CHEMIST, by THIX")
-            sleep(1)
-            print("Programmer: Daniel K.")
-            sleep(1)
-            print("Testers: Lianna K., Irina K., Daniel K., Drew Drew us")
-            sleep(1)
-            exit()
-        elif cmd=="5":
-            discovery_beaker.reset()
-            print("The beaker is now empty.")
-        else:
-            print("Sorry, but",cmd,"is not a valid command. Please try again.")
-        if not discovery_beaker.cont:
-            for n in range(0,100):
-                discovery_beaker.react(temperature,chemdict)
-            for n in discovery_beaker.chemicals:
-                if not n in discovery_beaker.discovered_chemicals:
-                    print("\nNew chemical discovered!",n)
-                    discovery_beaker.discovered_chemicals.append(n)
-            discovery_beaker.contents(temperature,chemdict)
-    
+def cap_first_letter(word):
+    word2=str()
+    try:
+        word2=word[0].upper()
+    except:
+        pass
+    for char in word[1:]:
+        word2=word2+char
+    return word2
+
+class TempError(Frame):
+    def __init__(self):
+        self.error=Tk()
+        super().__init__(self.error)
+        self.error.title("Error")
+
+        self.errortext=Label(self.error, text='''Please enter a number higher than -274
+and lower than 3501.''')
+        self.errortext.pack(side=TOP)
+
+        self.okbutton=Button(self.error, text="OK", command=lambda:self.error.destroy())
+        self.okbutton.pack(side=TOP)
+class GUI(Frame):
+    """The window for the game"""
+    def __init__(self, master, temp, chem):
+        super().__init__(master)
+        self.master=master
+        self.mods_in_mods=list()
+        self.mod_menu=list()
+        self.temp=20
+        self.chems=chem
+        self.reacts=beaker.reactions
+        self.init_widgets(temp, chem)
+    def init_widgets(self, temp, chem):
+        self.img=PhotoImage(file="Logo.png")
+        self.imglbl=Label(self.master, image=self.img)
+        self.imglbl.pack(side=TOP)
         
+        self.menubar=Menu(self.master)
+        
+        self.add=Menu(self.menubar)
+        self.catdict=dict()
+        self.categories=list(set([cap_first_letter(chem[chemical]["parent"]) for chemical in chem.keys()]))
+        self.categories.remove('Elements')
+        self.categories.insert(0, 'Elements')
+        self.categories.remove('Uncategorized')
+        self.categories.append('Uncategorized')
+        for category in self.categories:
+            self.catdict[category]=Menu(self.add)
+        for category in self.catdict.keys():
+            for chemical in chem.keys():
+                if chem[chemical]["parent"]==category.lower():
+                    self.catdict[category].add_command(label=chemical, command=lambda c=chemical:beaker.add(c))
+            self.add.add_cascade(label=category, menu=self.catdict[category])
+        
+        self.menubar.add_cascade(label="Add", menu=self.add)
+        
+        self.text=beaker.show_contents(temp, chem)
+        self.contents=Label(self.master, text=self.text)
+        self.contents.pack(side=TOP)
+
+        self.remove=Menu(self.menubar)
+        self.menubar.add_cascade(label="Remove", menu=self.remove)
+
+        self.others=Menu(self.menubar)
+        self.others.add_command(label="Change Temperature", command=lambda:self.create_temp_win())
+        self.others.add_command(label="Empty the Beaker", command=lambda:beaker.reset())
+        self.others.add_command(label="Credits", command=lambda:self.credits())
+        self.others.add_command(label="Exit", command=lambda:root.destroy())
+        self.menubar.add_cascade(label="Other Commands", menu=self.others)
+
+        self.mods=Menu(self.menubar)
+        with os.scandir(os.path.join(os.getcwd(), 'Mods')) as it:
+            for entry in it:
+                if '.json' in entry.name:
+                    self.mods_in_mods.append(entry.name)
+        self.mods_in_mods=[m.replace('.json', '') for m in self.mods_in_mods]
+        for mod in self.mods_in_mods:
+            self.mod_menu.append(Menu(self.mods))
+        for mod in range(0,len(self.mod_menu)):
+            self.mod_menu[mod].add_command(label="load", command=lambda m=self.mods_in_mods[mod]:self.mod_load(m))
+            self.mod_menu[mod].add_command(label="unload", command=lambda m=self.mods_in_mods[mod]:self.mod_unload(m))
+            self.mods.add_cascade(label=self.mods_in_mods[mod]+' mod', menu=self.mod_menu[mod])
+        self.menubar.add_cascade(label="Mods", menu=self.mods)
+        
+        root.config(menu=self.menubar)
+    def update_contents(self, temp, chem):
+        beaker.react(temperature, chemdict)
+        self.text=beaker.show_contents(temp, chem)
+        self.contents.config(text=self.text)
+        self.contents.pack(side=TOP)
+        self.remove.delete(0, 'end')
+        self.cmds_in_remove=beaker.contents
+        for chem in self.cmds_in_remove:
+            self.remove.add_command(label=chem, command=lambda n=chem:beaker.extract(n))
+    def credits(self):
+        self.creds=Tk()
+        self.creds.title("Credits")
+        self.credit_text=Label(self.creds, text='''
+Idea based on: BEAKER, by THIX; CHEMIST, by THIX
+Programmer: Daniel K.
+Testers: Lianna K., Irina K., Daniel K., Drew Drew us
+''')
+        self.credit_text.pack()
+    def create_temp_win(self):
+        self.tempinput=Tk()
+        self.tempinput.title("Change the Temperature")
+        
+        self.temptext=Label(self.tempinput, text="Enter the wanted temperature:")
+        self.temptext.pack(side=TOP)
+
+        self.tempentry=Entry(self.tempinput)
+        self.tempentry.pack(side=TOP)
+
+        self.okbutton=Button(self.tempinput, text="OK", command=lambda:self.change_temp())
+        self.okbutton.pack(side=TOP)
+    def change_temp(self):
+        self.temp=int(self.tempentry.get())
+        self.tempinput.destroy()
+        change_states(self.temp, chemdict)
+        if self.temp<-273 or self.temp>3500:
+            self.temp=temperature
+            TempError()
+    def mod_load(self, mod):
+        with open(os.path.join(os.getcwd(),'Mods',mod+'.json'), 'r') as f:
+            d=load(f)
+        with open('chemicals.json', 'r') as f:
+            c=load(f)
+        with open('reactions.json', 'r') as f:
+            r=load(f)
+        c.update(d["chemicals"])
+        r.update(d["reactions"])
+        with open('chemicals.json', 'w') as f:
+            dump(c, f, indent=4)
+        with open('reactions.json', 'w') as f:
+            dump(r, f, indent=4)
+    def mod_unload(self, mod):
+        with open(os.path.join(os.getcwd(),'Mods',mod+'.json'), 'r') as f:
+            d=load(f)
+        with open('chemicals.json', 'r') as f:
+            c=load(f)
+        with open('reactions.json', 'r') as f:
+            r=load(f)
+        c=dict_reduce(c, d["chemicals"])
+        r=dict_reduce(r, d["reactions"])
+        with open('chemicals.json', 'w') as f:
+            dump(c, f, indent=4)
+        with open('reactions.json', 'w') as f:
+            dump(r, f, indent=4)
+            
+
+with open("chemicals.json",'r') as r:
+    chemdict=load(r)
+temperature=20
+#The beaker
+beaker=Beaker(chemdict)
+root=Tk()
+root.geometry("1000x700")
+root.title("ChemMixer 4.0.0")
+gui=GUI(root, temperature, chemdict)
+#A try-except to catch an error when exiting
+while True:
+    try:
+        gui.update_contents(temperature, chemdict)
+        temperature=gui.temp
+        root.update()
+    except:
+        sys.exit()
--- game_funcs.py
+++ game_funcs.py
@@ -1,60 +1,43 @@
-def codify(text):
-    fi=text
-    fi=fi.strip()
-    fi=fi.lower()
-    return fi
-class CreativeBeaker():
+def dict_reduce(parent_dict, child_dict):
+    for key in child_dict.keys():
+        del parent_dict[key]
+    return parent_dict
+class Beaker():
     """A class to represent a beaker for creative mode"""
-    def __init__(self):
+    def __init__(self, chemdict):
         from json import load
-        self.chemicals=list()
-        reactions=dict()
+        self.dc=[chem for chem in chemdict.keys() if chemdict[chem]["element"]]
+        self.contents=list()
         with open("reactions.json",'r') as r:
             self.reactions=load(r)
         self.cont=False
-    def add(self,chemical,chemdict):
-        """Adds a chemical"""
-        if codify(chemical) in chemdict.keys():
-            self.chemicals.append(codify(chemical))
-        else:
-            self.cont=True
-    def extract(self,chemi):
-        """Removes a chemical"""
-        if chemi:
-            if chemi in self.chemicals:
-                self.chemicals.remove(chemi)
-            else:
-                self.cont=True
-        else:
-            print("\n\tSorry, but there is nothing to remove.\n")
+    def add(self, chem):
+        self.contents.append(chem)
+    def extract(self, chem):
+        if chem in self.contents:
+            del self.contents[self.contents.index(chem)]
     def reset(self):
         """Empties the beaker"""
-        for c in range(0,len(self.chemicals)+1):
-            self.chemicals=list()
+        self.contents=list()
     def react(self,temp,chemdict):
         """Checks for reactions and if there are any, reacts them"""
         tmp=0
         for a in self.reactions.keys():
             s1=set(self.reactions[a]["reactants"])
-            s2=set(self.chemicals)
+            s2=set(self.contents)
             if s1.issubset(s2) and (temp>self.reactions[a]["temp min"] and temp<=self.reactions[a]["temp max"]):
                 for c in self.reactions[a]["reactants"]:
                     self.extract(c)
                 for d in self.reactions[a]["results"]:
-                    self.add(d,chemdict)
-                print("\nA reaction has occurred.")
-    def contents(self,temp,chemdict):
+                    self.add(d)
+    def show_contents(self,temp,chemdict):
         """Shows the contents of the beaker"""
-        print("\nYour beaker is at ",temp,''' degrees Celsius and has the
-following chemicals inside:''')
-        for chemical in self.chemicals:
-            print(chemdict[chemical]["state"],chemical)
-class DiscoveryBeaker(CreativeBeaker):
-    def __init__(self):
-        super().__init__()
-        self.discovered_chemicals=["oxygen","hydrogen","nitrogen","helium",
-                                   "sulfur","aluminum","lithium","chlorine"
-                                   ]
+        txt="Your beaker is at "+str(temp)+''' degrees Celsius and has the
+following chemicals inside:'''
+        for chemical in self.contents:
+            txt+='\n'+chemdict[chemical]["state"]+' '+chemical
+        return txt
+
 def change_states(temp,chemdict):
     for chem in chemdict.keys():
         if temp>chemdict[chem]["melting_point"]:
@@ -65,10 +48,3 @@ def change_states(temp,chemdict):
             chemdict[chem]["state"]="liquid"
         if temp<=chemdict[chem]["melting_point"]:
             chemdict[chem]["state"]="solid"
-        
-        
-                  
-    
-        
-    
-            
